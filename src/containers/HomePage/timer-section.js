24fd9032ef418f34cda4fadffaef6fb2dfbcbf8a
David Costa <davidcostadev@gmail.com>
2019-07-03 08:59:29 -0300

    Improve timer style

--- src/containers/HomePage/timer-section.js
+++ src/containers/HomePage/timer-section.js
@@ -33,30 +33,28 @@ const Timer = () => {
     }
   }
 
-  if (currentState === 'playing' && duration >= 15 * 60 * 60 * 1000) {
+  if (currentState === 'playing' && duration >= 5 * 60 * 1000) {
     setCurrentState('ended')
     setDuration(0)
   }
 
   return (
     <div className="timer__container">
-      {currentState === 'ended' && (
-        <p>TIME OVER</p>
-      )}
-      {currentState !== 'ended' && (
-        <p>{moment.utc(duration).format('mm:ss')}</p>
-      )}
-      <div className="button__section">
+      <div className="timer__stopwatch">
+        {currentState === 'ended' && 'TIMEOVER'}
+        {currentState !== 'ended' && moment.utc(duration).format('mm:ss')}
+      </div>
+      <div className="timer__actions">
         {(currentState === 'stopped' || currentState === 'paused' || currentState === 'ended') && (
-          <button type="button" onClick={play}><i className="fa fa-play" /></button>
+          <button type="button" className="timer__button timer__button--play" onClick={play}><i className="fa fa-play" /></button>
         )}
         {currentState === 'playing' && (
-          <button type="button" onClick={pause}><i className="fa fa-pause" /></button>
+          <button type="button" className="timer__button timer__button--pause" onClick={pause}><i className="fa fa-pause" /></button>
         )}
         {(currentState === 'paused'|| currentState === 'playing') && (
-          <button type="button" onClick={stop}><i className="fa fa-stop" /></button>
+          <button type="button" className="timer__button timer__button--stop" onClick={stop}><i className="fa fa-stop" /></button>
         )}
-        <button className="testButton"><i className="fa fa-bolt" /></button>
+        <button className="timer__button timer__button--test"><i className="fa fa-bolt" /></button>
       </div>
     </div>
   );
