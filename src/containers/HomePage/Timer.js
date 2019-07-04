b87e702cf2d7b1519d63a48aba1ccccddae23f85
aj07mm <aj07mm@hotmail.com>
2019-07-04 17:31:29 -0300

    moving to class but not working

--- src/containers/HomePage/Timer.js
+++ src/containers/HomePage/Timer.js
@@ -2,66 +2,68 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import '../../stylesheets/timer.scss';
 
-
-const Timer = () => {
-  const ONE_SECOND = 1000;
-  const THRESHOLD = 5 * 60 * ONE_SECOND;
-
-  const [currentState, setCurrentState] = useState('stopped');
-  const [counter, setCounter] = useState(THRESHOLD);
-
-  const play = () => {
-    setCurrentState('playing');
+class Timer extends React.Component {
+  constructor(props){
+    super(props);
+    this.ONE_SECOND = 1000
+    this.THRESHOLD = 5 * 60 * ONE_SECOND
+    this.currentState, this.setCurrentState = useState('stopped');
+    this.counter, this.setCounter = useState(THRESHOLD);
   }
 
-  const stop = () => {
-    setCounter(THRESHOLD);
-    setCurrentState('stopped');
-  }
-
-  const pause = () => {
-    setCurrentState('paused');
-  }
-
-  useEffect(() => {
-    var timerID = setInterval(() => tick(), ONE_SECOND);
-
-    return function cleanup() {
-      clearInterval(timerID);
-    };
-  });
-
-  function tick() {
-    if (currentState === 'playing') {
-      setCounter(counter - ONE_SECOND);
+  componentDidMount() {
+    useEffect(() => {
+        return function cleanup() {
+            clearInterval(setInterval(() => this.tick(), this.ONE_SECOND));
+        };
+    });
+    if (currentState === 'playing' && this.counter === 0) {
+        this.setCurrentState('ended');
+        this.setCounter(THRESHOLD);
     }
   }
 
-  if (currentState === 'playing' && counter === 0) {
-    setCurrentState('ended');
-    setCounter(THRESHOLD);
+  play() {
+    setCurrentState('playing');
   }
 
-  return (
-    <div className="timer__container">
-      <div className="timer__stopwatch">
-        {currentState === 'ended' && 'TIMEOVER'}
-        {currentState !== 'ended' && moment.utc(counter).format('mm:ss')}
-      </div>
-      <div className="timer__actions">
-        {(currentState === 'stopped' || currentState === 'paused' || currentState === 'ended') && (
-          <button type="button" className="timer__button timer__button--play" onClick={play}><i className="fa fa-play" /></button>
-        )}
-        {currentState === 'playing' && (
-          <button type="button" className="timer__button timer__button--pause" onClick={pause}><i className="fa fa-pause" /></button>
-        )}
-        {(currentState === 'paused'|| currentState === 'playing') && (
-          <button type="button" className="timer__button timer__button--stop" onClick={stop}><i className="fa fa-stop" /></button>
-        )}
-        <button className="timer__button timer__button--test"><i className="fa fa-bolt" /></button>
-      </div>
-    </div>
-  );
+  stop() {
+    this.setCounter(THRESHOLD);
+    this.setCurrentState('stopped');
+  }
+
+  pause() {
+    this.setCurrentState('paused');
+  }
+
+  tick() {
+    if (currentState === 'playing') {
+      this.setCounter(this.counter - this.ONE_SECOND);
+    }
+  }
+
+  render(){
+    return (
+        <div className="timer__container">
+        <div className="timer__stopwatch">
+            {currentState === 'ended' && 'TIMEOVER'}
+            {currentState !== 'ended' && moment.utc(this.counter).format('mm:ss')}
+        </div>
+        <div className="timer__actions">
+            {(currentState === 'stopped' || currentState === 'paused' || currentState === 'ended') && (
+            <button type="button" className="timer__button timer__button--play" onClick={this.play}><i className="fa fa-play" /></button>
+            )}
+            {currentState === 'playing' && (
+            <button type="button" className="timer__button timer__button--pause" onClick={this.pause}><i className="fa fa-pause" /></button>
+            )}
+            {(currentState === 'paused'|| currentState === 'playing') && (
+            <button type="button" className="timer__button timer__button--stop" onClick={this.stop}><i className="fa fa-stop" /></button>
+            )}
+            <button className="timer__button timer__button--test"><i className="fa fa-bolt" /></button>
+        </div>
+        </div>
+    );
+  }
 };
 
 
